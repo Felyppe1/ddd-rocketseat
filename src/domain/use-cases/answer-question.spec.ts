36e89dd9622f7c1bca74d8c714c014410720fe44
Felyppe Nunes <121322408+Felyppe1@users.noreply.github.com>
2024-08-01 00:50:13 +0000

    feat: answer question use case

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,18 @@
 import { expect, test } from 'vitest'
 import { AnswerQuestionUseCase } from './answer-question'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { Answer } from '../entities/answer'
 
-test('it should create an answer', () => {
-    const answerQuestion = new AnswerQuestionUseCase()
+const fakeAnswersRepository: AnswersRepository = {
+    create: async (answer: Answer) => {
+        return
+    }
+}
 
-    const answer = answerQuestion.execute({
+test('it should create an answer', async () => {
+    const answerQuestionUseCase = new AnswerQuestionUseCase(fakeAnswersRepository)
+
+    const answer = await answerQuestionUseCase.execute({
         questionId: '1',
         instructorId: '1',
         content: 'Nova Resposta'
